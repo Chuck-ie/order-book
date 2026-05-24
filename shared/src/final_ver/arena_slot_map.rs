07e76ddf060e4842cf09c06856beb4c415f1f501
Chuck-ie <carneintom1@gmail.com>
2026-05-25 01:37:11 +0200

    add insert and remove functions for arena slotmap

--- shared/src/final_ver/arena_slot_map.rs
+++ shared/src/final_ver/arena_slot_map.rs
@@ -12,11 +12,9 @@ pub struct ArenaSlotMap {
     pub tail: NonMaxU32,
     pub free_head: NonMaxU32,
     pub owned_chunks: Vec<ArenaChunkIndex>,
-    // pub capacity: usize,
-    pub next_unallocated: u32,
     pub total_capacity: usize,
     pub total_len: usize,
-    // pub current_len: usize,
+    pub total_occupied: usize,
 }
 
 impl ArenaSlotMap {
@@ -35,118 +33,128 @@ impl ArenaSlotMap {
             owned_chunks,
             total_capacity: chunk_size,
             total_len: 0,
-            // current_len: 0,
+            total_occupied: 0,
         }
     }
 
+    // ex1: insert into empty
+
     #[allow(clippy::cast_possible_truncation)]
     pub fn insert(&mut self, data: LimitOrder, arena: &mut ArenaSlotAllocator) -> ArenaIndex {
-        let free_index;
+        let free_index = self.free_head;
+        // we have a slot to recycle
+        let insert_index = if free_index.is_some() {
+            let (_, next_free) = unsafe {
+                arena
+                    .get_unchecked(free_index.0 as usize)
+                    .as_free_unchecked()
+            };
 
-        if self.free_head.is_some() {
-            free_index = self.free_head.0;
-
-            unsafe {
-                let (_, next_free) = arena.get_unchecked(free_index as usize).as_free_unchecked();
-                self.free_head = *next_free;
-            }
+            self.free_head = *next_free;
+            free_index.0
+        // we need to check if we need to allocate a new chunk of slots
+        // case1: allocate slot + write to index 0
+        // case2: allocate to the latest index
         } else {
-            if self.current_len == arena.chunk_size() {
-                // TODO: benchmark if hint::cold_path reduce branch misses even further
-                // since resizing should happen rarely
+            // 1024 means we need a new chunk
+            let chunk_offset = self.total_len % arena.chunk_size();
+
+            if chunk_offset == 0 {
                 let new_chunk_index = unsafe { arena.claim_chunk() };
                 self.owned_chunks.push(new_chunk_index);
-                self.capacity += arena.chunk_size();
-                self.current_len = 0;
+                self.total_capacity += arena.chunk_size();
             }
 
-            let Some(free_chunk_index) = self.owned_chunks.last() else {
+            let Some(last_chunk_index) = self.owned_chunks.last() else {
                 unsafe { std::hint::unreachable_unchecked() };
             };
 
-            free_index = (free_chunk_index.0 * arena.chunk_size() + self.current_len) as u32;
-            self.current_len += 1;
-        }
+            self.total_len += 1;
+            (last_chunk_index.0 * arena.chunk_size() + chunk_offset) as u32
+        };
 
         let tail_index = self.tail;
 
         if tail_index.is_some() {
-            unsafe {
-                let (_, _, _, next) = arena
+            let (_, _, _, next) = unsafe {
+                arena
                     .get_unchecked_mut(tail_index.0 as usize)
-                    .as_occupied_unchecked_mut();
+                    .as_occupied_unchecked_mut()
+            };
 
-                next.0 = free_index;
-            }
+            next.0 = insert_index;
         }
 
-        let new_occupied = ArenaSlot::occupied_with_prev(data, tail_index);
-
-        unsafe {
-            let free_slot_ref = arena.get_unchecked_mut(free_index as usize);
-            *free_slot_ref = new_occupied;
-        };
+        let insert_slot_ref = unsafe { arena.get_unchecked_mut(insert_index as usize) };
+        *insert_slot_ref = ArenaSlot::occupied_with_prev(data, tail_index);
 
         if self.head.is_none() {
-            self.head.0 = free_index;
+            self.head.0 = insert_index;
         }
 
-        self.tail.0 = free_index;
-        self.total_len += 1;
+        self.tail.0 = insert_index;
+        self.total_occupied += 1;
 
-        ArenaIndex(free_index)
+        ArenaIndex {
+            generation: 0,
+            index: insert_index,
+        }
     }
 
     pub fn remove(&mut self, remove_index: &ArenaIndex, arena: &mut ArenaSlotAllocator) -> bool {
-        // TODO: honestly, this should probably never be unsafe. Slots should have a version index and
-        // then this initial fetch should check if slot is occupied and version of slot and index match
-        let (_, _, curr_prev, curr_next) = unsafe {
-            arena
-                .get_unchecked(remove_index.0 as usize)
-                .as_occupied_unchecked()
+        let (generation, curr_prev, curr_next) = {
+            let Some(ArenaSlot::Occupied {
+                generation,
+                prev,
+                next,
+                ..
+            }) = arena.get(remove_index.index as usize)
+            else {
+                return false;
+            };
+
+            if *generation != remove_index.generation {
+                return false;
+            }
+
+            (*generation, *prev, *next)
         };
 
-        let curr_prev = *curr_prev;
-        let curr_next = *curr_next;
-
         if curr_prev.is_some() {
-            unsafe {
-                let (_, _, _, next) = arena
+            let (_, _, _, next) = unsafe {
+                arena
                     .get_unchecked_mut(curr_prev.0 as usize)
-                    .as_occupied_unchecked_mut();
+                    .as_occupied_unchecked_mut()
+            };
 
-                *next = curr_next;
-            }
-        }
-
-        if curr_next.is_some() {
-            unsafe {
-                let (_, _, prev, _) = arena
-                    .get_unchecked_mut(curr_next.0 as usize)
-                    .as_occupied_unchecked_mut();
-                *prev = curr_prev;
-            }
-        }
-
-        unsafe {
-            let remove_slot_ref = arena.get_unchecked_mut(remove_index.0 as usize);
-            *remove_slot_ref = ArenaSlot::free_with_next(self.free_head);
-        }
-
-        self.free_head = NonMaxU32::from(remove_index.0);
-
-        if curr_next.is_none() {
-            self.tail = curr_prev;
-        }
-
-        if curr_prev.is_none() {
+            *next = curr_next;
+        } else {
             self.head = curr_next;
         }
 
-        self.total_len = self.total_len.saturating_sub(1);
+        if curr_next.is_some() {
+            let (_, _, prev, _) = unsafe {
+                arena
+                    .get_unchecked_mut(curr_next.0 as usize)
+                    .as_occupied_unchecked_mut()
+            };
 
-        // return value should be used to release the claims on all chunks allocated by this slotmap
-        self.total_len == 0
+            *prev = curr_prev;
+        } else {
+            self.tail = curr_prev;
+        }
+
+        unsafe {
+            let remove_slot_ref = arena.get_unchecked_mut(remove_index.index as usize);
+            *remove_slot_ref = ArenaSlot::Free {
+                generation,
+                next_free: self.free_head,
+            }
+        }
+
+        self.free_head.0 = remove_index.index;
+        self.total_occupied -= 1;
+        self.total_occupied == 0
     }
 }
 
