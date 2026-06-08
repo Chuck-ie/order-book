357e8316cf3d7386d905f1acb5c17c4e015c76a7
Chuck-ie <carneintom1@gmail.com>
2026-06-08 22:32:43 +0200

    give up on MPMC for now...

--- src/channel/producer.rs
+++ src/channel/producer.rs
@@ -1,55 +1,94 @@
 use std::{mem::MaybeUninit, sync::atomic::Ordering};
 
 use crate::channel::{
-    buffer_handle::{BufferHandle, MP},
+    buffer_handle::{BufferHandle, FromBuffer, MP},
     spinlock::Spinlock,
 };
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ProducerError {
+    QueueFull,
+    Timeout,
+}
 
 pub trait Producer {
     type Item;
 
-    fn try_write(&self, value: Self::Item) -> bool;
+    fn try_write(&self, value: Self::Item) -> Result<(), ProducerError>;
+}
+
+impl<T> Clone for BufferHandle<T, MP> {
+    fn clone(&self) -> Self {
+        Self::new(self.inner())
+    }
 }
 
 impl<T> Producer for BufferHandle<T, MP> {
     type Item = T;
 
-    fn try_write(&self, value: Self::Item) -> bool {
-        let mut spinlock = Spinlock::new();
-        let mut curr_write_index = self.head.load(Ordering::Relaxed);
+    /*
+     *  (000 + 001) & (011) -> 001 & 011 = 001
+     *  (001 + 001) & (011) -> 010 & 011 = 010
+     *  (010 + 001) & (011) -> 011 & 011 = 011
+     *  (011 + 001) & (011) -> 100 & 011 = 000
+     *  (100 + 001) & (011) -> 101 & 011 = 001
+     *  (101 + 001) & (011) -> 110 & 011 = 010
+     *
+     *
+     *  division? !011 = 100
+     *  0 000 & 100 = 000
+     *  1 001 & 100 = 000
+     *  2 010 & 100 = 000
+     *  3 011 & 100 = 000
+     *  4 100 & 100 = 100
+     *  81000 & 100 = 000
+     */
 
-        loop {
-            let curr_read_index = self.tail.load(Ordering::Relaxed);
-
-            // this is a faster modulo operation, which only works under the assumption that N is a power of 2
-            if (curr_write_index + 1) & self.capacity == curr_read_index {
-                return false;
-            }
-
-            let next_write_index = (curr_write_index + 1) & self.capacity;
-
-            match self.head.compare_exchange_weak(
-                curr_write_index,
-                next_write_index,
-                Ordering::SeqCst,
-                Ordering::Relaxed,
-            ) {
-                Ok(_) => {
-                    unsafe {
-                        let write_ptr = self.buffer.get_unchecked(curr_write_index).get();
-                        write_ptr.write(MaybeUninit::new(value));
-                    }
-
-                    return true;
-                }
-                Err(updated_write_index) => {
-                    curr_write_index = updated_write_index;
-
-                    if !spinlock.spin() {
-                        return false;
-                    }
-                }
-            }
-        }
+    fn try_write(&self, value: Self::Item) -> Result<(), ProducerError> {
+        todo!();
+        // let inner = self.inner();
+        // let mut spinlock = Spinlock::new();
+        // let mut curr_write_index = inner.head.load(Ordering::Relaxed);
+        //
+        // loop {
+        //     let curr_read_index = inner.tail.load(Ordering::Relaxed);
+        //
+        //     // this is a faster modulo operation, which only works under the assumption that N is a power of 2
+        //     let plus_one = curr_write_index + 1;
+        //     let next_write_index = plus_one & inner.capacity;
+        //
+        //     if next_write_index == curr_read_index {
+        //         return Err(ProducerError::QueueFull);
+        //     }
+        //
+        //     let slot = unsafe { inner.buffer.get_unchecked(curr_write_index) };
+        //     let ticket = slot.ticket.load(Ordering::Acquire);
+        //
+        //     match inner.head.compare_exchange_weak(
+        //         curr_write_index,
+        //         next_write_index,
+        //         Ordering::SeqCst,
+        //         Ordering::Relaxed,
+        //     ) {
+        //         Ok(_) => {
+        //             // Safety
+        //             //
+        //             // we make sure above that we sync other threads by aquiring access and only
+        //             // releasing it after the write happened
+        //             unsafe {
+        //                 slot.write(MaybeUninit::new(value));
+        //             }
+        //
+        //             return Ok(());
+        //         }
+        //         Err(updated_write_index) => {
+        //             curr_write_index = updated_write_index;
+        //
+        //             if !spinlock.spin() {
+        //                 return Err(ProducerError::Timeout);
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
--- src/channel/ring_buffer.rs
+++ src/channel/ring_buffer.rs
@@ -1,6 +1,51 @@
 use std::{cell::UnsafeCell, mem::MaybeUninit, ops::Deref, sync::atomic::AtomicUsize};
 
-pub type BufferSlot<T> = CacheLinePadded<UnsafeCell<MaybeUninit<T>>>;
+#[repr(align(64))]
+#[derive(Default)]
+pub struct CacheLinePadded<T>(T);
+
+impl<T> Deref for CacheLinePadded<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[repr(align(64))]
+pub struct BufferSlot<T> {
+    cell: UnsafeCell<MaybeUninit<T>>,
+    pub(crate) ticket: AtomicUsize,
+}
+
+impl<T> BufferSlot<T> {
+    #[inline]
+    pub const fn from_ticket(ticket: usize) -> Self {
+        Self {
+            cell: UnsafeCell::new(MaybeUninit::uninit()),
+            ticket: AtomicUsize::new(ticket),
+        }
+    }
+
+    /// # Safety
+    ///
+    /// The caller has to make sure that writing is allowed and wont cause any race conditions with other threads
+    #[inline]
+    pub const unsafe fn write(&self, value: MaybeUninit<T>) {
+        unsafe { self.cell.get().write(value) };
+    }
+
+    /// # Safety
+    ///
+    /// The caller has to make sure that reading is allowed and wont cause any race conditions with other threads
+    #[inline]
+    pub const unsafe fn read(&self) -> T {
+        unsafe { self.cell.get().read().assume_init_read() }
+    }
+
+    // let read_ptr = inner.buffer.get_unchecked(curr_read_index).get();
+    // read_ptr.read().assume_init_read()
+}
 
 // TODO: check if const BUF_SIZE is preferred here. Originally I planned to use it for a fixed size,
 // but heap allocated array, but that might cause problems so now its a dynamic size, but fixed at
@@ -13,6 +58,9 @@ pub struct RingBuffer<T> {
     pub(crate) capacity: usize,
 }
 
+unsafe impl<T: Send> Send for RingBuffer<T> {}
+unsafe impl<T: Sync> Sync for RingBuffer<T> {}
+
 impl<T> RingBuffer<T> {
     /// # Panics
     ///
@@ -22,9 +70,7 @@ impl<T> RingBuffer<T> {
         assert!(capacity > 1, "capacity must be greater than 1");
         assert!(capacity.is_power_of_two(), "capacity must be a power of 2");
 
-        let buffer = (0..capacity)
-            .map(|_| CacheLinePadded(UnsafeCell::new(MaybeUninit::uninit())))
-            .collect();
+        let buffer = (0..capacity).map(|i| BufferSlot::from_ticket(i)).collect();
 
         Self {
             buffer,
@@ -50,16 +96,4 @@ impl<T> RingBuffer<T> {
      *   ^r    ^w
      *  -> w CAN write, r CAN read
      */
-}
-
-#[repr(align(64))]
-#[derive(Default)]
-pub struct CacheLinePadded<T>(T);
-
-impl<T> Deref for CacheLinePadded<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
 }
